4d059687c99c058555808a0938f69e91bdfc1ee0
John-Onome <87643704+John-Onome@users.noreply.github.com>
2021-08-20 18:20:20 +0100

    Task one

---
+++ task.js
@@ -0,0 +1,12 @@
+let bioData = {
+	firstName: 'Onome',
+	lastName: 'John-Polo',
+	nickName: '(Nomzy)',
+	gender: 'Female',
+	age: 26,
+	nationality: 'Nigerian',
+	stateOfOrigin: 'Rivers',
+	maritalStatus: 'Single',
+	tribe: 'Engenni',
+}
+console.log(bioData);
